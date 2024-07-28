18708d371ac9487d685343badb007096c05b1500
Kerem Mimir <mimirkerem@gmail.com>
2024-07-28 17:30:12 +0300

    InternTasks-3A | Add counter to Fragment and ViewModel

--- app/src/main/java/com/example/interntasks_3a/CounterFragment.kt
+++ app/src/main/java/com/example/interntasks_3a/CounterFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.interntasks_3a.databinding.FragmentCounterBinding
 
 
 class CounterFragment : Fragment() {
     private var _binding: FragmentCounterBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: CounterViewModel by viewModels()
+
+    private var counter = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +28,40 @@ class CounterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.switch1.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.counterText.text=viewModel.viewModelCounter.toString()
+
+                binding.counterButton.setOnClickListener {
+                    viewModel.counterNumber()
+                    binding.counterText.text=viewModel.viewModelCounter.toString()
+                }
+            } else {
+                binding.counterText.text=counter.toString()
+
+                binding.counterButton.setOnClickListener {
+                    counter++
+                    binding.counterText.text=counter.toString()
+                }
+            }
+        }
+
+        if (binding.switch1.isChecked){
+            binding.counterButton.setOnClickListener {
+                viewModel.counterNumber()
+                binding.counterText.text=viewModel.viewModelCounter.toString()
+            }
+        }else{
+            binding.counterButton.setOnClickListener {
+                counter++
+                binding.counterText.text=counter.toString()
+            }
+        }
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/example/interntasks_3a/CounterViewModel.kt
+++ app/src/main/java/com/example/interntasks_3a/CounterViewModel.kt
@@ -3,4 +3,10 @@ package com.example.interntasks_3a
 import androidx.lifecycle.ViewModel
 
 class CounterViewModel : ViewModel() {
+
+    var viewModelCounter = 0
+
+    fun counterNumber(){
+        viewModelCounter++
+    }
 }
