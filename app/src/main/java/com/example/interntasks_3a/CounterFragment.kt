67cedbdc8ca3c3b66b5c9faf232f2062c1ee41db
Kerem Mimir <mimirkerem@gmail.com>
2024-07-28 16:12:20 +0300

    InternTasks-3A | Add Fragment, ViewModel and layout design

---
+++ app/src/main/java/com/example/interntasks_3a/CounterFragment.kt
@@ -0,0 +1,28 @@
+package com.example.interntasks_3a
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.interntasks_3a.databinding.FragmentCounterBinding
+
+
+class CounterFragment : Fragment() {
+    private var _binding: FragmentCounterBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentCounterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
